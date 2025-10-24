32f7133e572124b29510df63101f7b54fc9f9bf2
mattia-lavello <mattia.lavello@aldini.istruzioneer.it>
2025-10-24 07:40:10 +0000

    prova di scrittura seguita da lettura

---
+++ LeggiScriviFile/src/App.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*
+        //prima scrivo dentro un file
+        FileWriter myWriter = new FileWriter("nome.txt");
+        for(int i=0; i<100; i++) {
+            myWriter.write("Questa è una prima scritta in un file.\n");
+            
+        }
+        myWriter.close();
+        System.out.println("Ho scritto sul file.");
+        
+        //ora leggo sul file
+        File f1 = new File("nome.txt");
+        Scanner input = new Scanner(f1);
+        while(input.hasNextLine()) {
+            String s1 = input.nextLine();
+            System.out.println(s1);
+        }
+        input.close();
+        */
+
+        
+    }
+}
